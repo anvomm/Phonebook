15ef8abd74809a5f8545625258b083a4cececc00
Anastassia Vomm <anastassia.vomm@gmail.com>
2023-01-08 18:22:16 +0200

    stylization

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,24 +2,36 @@ import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Loader } from 'components/Loader/Loader';
 import { Navigation } from 'components/Navigation/Navigation';
-import { Box } from '@chakra-ui/react';
+import { Box, Text } from '@chakra-ui/react';
 
 export const Layout = () => (
   <Box
     bgGradient="linear(287.15deg, #450E4B 0%, #3C0C41 24.48%, rgba(207, 0, 99, 0) 100%)"
     bgColor={'#CF0063'}
     h={'100vh'}
+    display="flex"
+    flexDirection={'column'}
   >
-    <header>
-      <Box w={1200} mx="auto" pt={30}>
-        <Navigation />
-      </Box>
-    </header>
+    <Box as="header" w={1200} mx="auto" pt={30}>
+      <Navigation />
+    </Box>
     <main>
       <Suspense fallback={<Loader />}>
         <Outlet />
       </Suspense>
     </main>
-    <footer>footer</footer>
+    <Box as="footer" mt={'auto'} pb={'15px'}>
+      <Box
+        w={1200}
+        mx="auto"
+        pt={30}
+        display={'flex'}
+        justifyContent={'flex-end'}
+      >
+        <Text color={'#fff'} fontSize={'13px'}>
+          @2023 Phonebook. Created by anvomm.
+        </Text>
+      </Box>
+    </Box>
   </Box>
 );
