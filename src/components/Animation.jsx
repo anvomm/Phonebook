3970e8f7374d5177d9849b6880ecc2378172728f
Anastassia Vomm <anastassia.vomm@gmail.com>
2023-01-11 00:30:36 +0200

    animation speeded up

--- src/components/Animation.jsx
+++ src/components/Animation.jsx
@@ -1,15 +1,14 @@
 import * as React from "react";
 import { useState } from "react";
-import { book, phone, mobile/* , contact */ } from "../utils/paths";
+import { book, phone, mobile } from "../utils/paths";
 import { motion, useMotionValue, useTransform, animate } from "framer-motion";
 import { getIndex, useFlubber } from "../hooks/useFlubber.ts";
 
-const paths = [ phone, /* contact, */ mobile, book, phone];
+const paths = [ phone, mobile, book, phone];
 const colors = [
     "#8855ff",
   "#0099ff",
   "#ff0055",
-  /* "#ee4444", */
   "#00cc88"
 ];
 
@@ -22,14 +21,14 @@ export function Animation() {
     React.useEffect(() => {
       const animation = animate(progress, pathIndex, {
           duration: 0.8,
-          delay: 1,
         ease: "easeInOut",
         onComplete: () => {
           if (pathIndex === paths.length - 1) {
             progress.set(0);
             setPathIndex(1);
           } else {
-            setPathIndex(pathIndex + 1);
+              setPathIndex(pathIndex + 1);
+              
           }
         }
       });
@@ -39,7 +38,7 @@ export function Animation() {
     return (
         <svg width="350" height="350" viewBox="0 0 500 500" style={{paddingTop: '60px'}} >
         <g transform="translate(10 10) scale(17 17)">
-                <motion.path transition= {{ delay: 0, }} fill={fill} d={path} />
+                <motion.path fill={fill} d={path} />
         </g>
       </svg>
     );
--- src/hooks/useFlubber.ts
+++ src/hooks/useFlubber.ts
@@ -5,6 +5,6 @@ export const getIndex = (_: any, index: number) => index;
 
 export function useFlubber(progress: MotionValue<number>, paths: string[]) {
   return useTransform(progress, paths.map(getIndex), paths, {
-    mixer: (a, b) => flubber.interpolate(a, b, { maxSegmentLength: 0.1}),
+    mixer: (a, b) => flubber.interpolate(a, b, { maxSegmentLength: 0.3}),
   });
 }
